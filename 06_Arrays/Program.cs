3a7732a072d62cddcc92a55f8a9c4b56364a4d55
muratbulca <112632315+muratbulca@users.noreply.github.com>
2024-10-31 10:04:31 +0300

    CSharpEgitimKampi
    10. Ders

---
+++ 06_Arrays/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Temel Dizi Örnekleri
+            //string[] colors = new string[4];
+            //colors[0] = "Kırmızı";
+            //colors[1] = "Beyaz";
+            //colors[2] = "Siyah";
+            //colors[3] = "Mavi";
+            //Console.WriteLine(colors[3]);
+
+            //***************************************
+
+            //string[] cities = new string[5];
+            //cities[0] = "Madrid";
+            //cities[1] = "Atina";
+            //cities[2] = "Moskova";
+            //cities[3] = "Tiran";
+            //cities[4] = "Ankara";
+
+            //Console.WriteLine(cities[4]);
+
+            //***************************************
+
+            //int[] numbers = new int[10];
+            //numbers[0] = 54;
+            //numbers[1] = 16;
+            //numbers[2] = 34;
+            //numbers[4] = 794;
+            //numbers[8] = 921;
+
+            //Console.WriteLine(numbers[8]);
+
+            //***************************************
+
+            //string[] cities = { "Prag", "Roma", "Atina", "Ankara", "Bursa" };
+            //Console.WriteLine(cities[4]);
+            #endregion
+
+            #region Dizideki Tüm Elemanları Listeleme
+
+            //string[] colors = { "Sarı", "Mavi", "Siyah", "Beyaz", "Kırmızı", "Truncu", "Yeşil" };
+
+            //for (int i = 0; i < colors.Length; i++)
+            //{
+            //    Console.WriteLine(colors[i]);
+            //}
+
+            //***************************************
+
+            //int[] numbers = { 5, 9, 54, 83, 234, 532, 646, 982, 7846, 325, -27, 8664 };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] %3 == 0)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+
+            //***************************************
+
+            //char[] symbols = { 'a', 'b', 'c', '*', '#', '&' };
+            //for (int i = 0; i < symbols.Length; i++)
+            //{
+            //    Console.WriteLine(symbols[i]);
+            //}
+
+            //***************************************
+
+            //int[] myArray = { 3, 8, 76, 54, 81, 843, 548, 99, 7493, 633, 3342 };
+            //int maxNumber = myArray[0];
+            //for (int i = 0;  i < myArray.Length; i++)
+            //{
+            //    if (myArray[i] > maxNumber)
+            //    {
+            //        maxNumber = myArray[i];
+            //    }
+            //}
+            //Console.WriteLine(maxNumber);
+
+            //***************************************
+
+            //string[] persons = { "ali", "Fatma", "Kemal", "Zeynep", "Buse", "Cem", "Hakan" };
+            //Console.WriteLine(persons.Length);
+
+            //***************************************
+
+            //int[] numbers = { 54, 64, 32, 11, 75, 643, 69, 434, 654, 98, 450, 8543 };
+            //Array.Sort(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            //***************************************
+
+            //int[] numbers = { 54, 64, 32, 11, 75, 643, 69, 434, 654, 98, 450, 8543 };
+            //Array.Reverse(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            #endregion
+
+            #region Dizi Metotları
+
+            //string[] customers= { "ali", "Fatma", "Kemal", "Zeynep", "Buse", "Cem", "Hakan" };
+            //int index = Array.IndexOf(customers, "Zeynep");
+            //Console.WriteLine(index);
+
+            //************************************************
+
+            //int[] numbers = { 44, 65, 54, 77, 87, 22, 9, 74, 32, 107 };
+            //Console.WriteLine("Dizinin En Büyük Elemanı: " + numbers.Max() + " Dizinin En Küçük Elemanı: " + numbers.Min());
+            #endregion
+
+            #region Kullanıcıdan Değer Alma
+
+            //string[] cities = new string[5];
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.Write($"Lütfen {i + 1}. Şehri Giriniz: ");
+            //    cities[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("---------------------");
+            //for (int i = 0;i < cities.Length;i++)
+            //{
+            //    Console.WriteLine(cities[i]);
+            //}
+
+            //*****************************************
+
+            //int[] numbsers = { 10, 20, 30, 40, 50, 60 };
+            //int sum = 0;
+            //for(int i = 0; i < numbsers.Length; i++)
+            //{ 
+            //    sum += numbsers[i]; 
+            //}
+            //Console.WriteLine(sum);
+
+            //*****************************************
+
+            int[] numbers = { 21, 42, 63, 64, 875, 436, 357, 248, 449, 5310 };
+            Console.WriteLine("Çift Sayılar");
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] %2 == 0) 
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Tek Sayılar");
+            Console.WriteLine();
+
+            for(int i = 0;i < numbers.Length; i++)
+            {
+                if (numbers[i] %2 == 1)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+            #endregion
+            Console.Read();
+        }
+    }
+}
---
+++ 07_ForeachLoops/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach Döngüsü
+
+            //string[] cities = { "milano", "roma", "Atina", "Ankara", "Prag" };
+            //foreach (string x in cities)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //******************************************
+
+            //int[] numbsers = { 43, 6, 43, 646, 63, 87, 24, 768, 644, 7674, 2423, 42, 678 };
+            //foreach (int number in numbsers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //*****************************************
+
+            //int[] numbsers = { 43, 6, 43, 646, 63, 87, 24, 768, 644, 7674, 2423, 42, 678 };
+            //foreach (int number in numbsers)
+            //{
+            //    if (number %2 == 0)
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+            //}
+
+            //******************************************
+
+            //int[] numbsers = { 43, 6, 43, 646, 63, 87, 24, 768, 644, 7674, 2423, 42, 678 };
+            //int total = 0;
+
+            //foreach (int num in numbsers)
+            //{
+            //    total += num;
+            //}
+            //Console.WriteLine(total);
+
+            //*********************************************
+
+            //List<int> numbers = new List<int>()
+            //{
+            //    1,2,4,5,8
+            //};
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //*********************************************
+
+            //string word = "Merhaba";
+
+            //foreach(char c in word)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            #endregion
+
+            #region Örnek Sınav Sistemi Uygulaması
+
+            Console.Write("***** C# Eğitim Kampı Sınav Uygulaması *****");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //sınıftaki öğrenci sayısını kullanıcıdan alma
+            Console.WriteLine("--------------------------------");
+            Console.Write("Sınıfınızda Kaç Öğrenci Var: ");
+            int studentCount=int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------------");
+
+            //Öğrenci isimlerini ve not ortalamalarını saklayacak diziller
+            string[] studentNames = new string[studentCount];
+            double[] studentExamAvg = new double[studentCount];
+
+            for(int i = 0; i < studentCount; i++)
+            {
+                Console.Write($"{i + 1}.öğrencinin ismini giriniz: ");
+                studentNames[i] = Console.ReadLine();
+
+                double totalExamResult = 0;
+
+                // Her öğrenci için 3 sınav notu girişi
+                for(int j = 0;j<3; j++)
+                {
+                    Console.Write($"{studentNames[i]} isimli öğrencinin {j + 1}. sınav notunu giriniz: ");
+                    double value = double.Parse(Console.ReadLine());
+                    totalExamResult += value; // notları topluyoruz
+                }
+                Console.WriteLine();
+                studentExamAvg[i] = totalExamResult /3;
+            }
+            //Sınav ortalamarı
+            for(int i = 0;i < studentCount;i++)
+            {
+                Console.WriteLine("-----------------------");
+                Console.WriteLine($"{studentNames[i]} adlı öğrencinin ortalaması: {studentExamAvg[i]}");
+                //Öğrencilerin ortalaması ve geçip kalma durumları
+                if (studentExamAvg[i] >= 50) 
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersi geçti");
+                }
+                else
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersten kaldı");
+                }
+                Console.WriteLine("-----------------------");
+            }
+            #endregion
+            Console.Read();
+        }
+    }
+}
